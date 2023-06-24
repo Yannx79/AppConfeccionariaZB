b4a9078e440d152ef073a3bc0d2f6ba4a5f43f90
Yannick Funes <yannfscz@gmail.com>
2023-06-24 12:10:55 -0500

    MBO: Organizacion BO

--- src/main/java/com/nk/models/datasource/IConexion.java
+++ src/main/java/com/nk/models/datasource/IConexion.java
@@ -6,7 +6,7 @@ public interface IConexion {
 
     public final String USER = "root";
     public final String PASSWORD = "";
-    public final String URL = "jdbc:mysql://localhost:3306/db_veterinaria";
+    public final String URL = "jdbc:mysql://localhost:3306/db_confeccionario";
     public final String DRIVER = "com.mysql.cj.jdbc.Driver";
 
     public abstract Connection getConnection();
