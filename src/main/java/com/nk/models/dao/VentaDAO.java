115020c365e4467abb859f232b608d6efa072357
[Jose Cardenas] <[josejesuscardenasjimenez@gmail.com]>
2023-06-25 20:40:23 -0500

    Medio avance de VentaDAO(por si a caso)

--- src/main/java/com/nk/models/dao/VentaDAO.java
+++ src/main/java/com/nk/models/dao/VentaDAO.java
@@ -1,6 +1,94 @@
-
 package com.nk.models.dao;
 
-public class VentaDAO {
+import com.nk.models.dto.VentaDTO;
+import com.nk.models.idao.IVentaDAO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.LinkedList;
+import java.util.List;
+
+public class VentaDAO implements IVentaDAO<VentaDTO, Integer>{
+
+    @Override
+    public boolean create(VentaDTO t) {
+        boolean isCreated = false;
+        try {
+            Connection connection = CONEXION.getConnection();
+            PreparedStatement ps = connection.prepareStatement(SQL_CREATE);
+            ps.setInt(1, t.getId_usuario());
+            ps.setInt(2, t.getId_cliente());
+            ps.setDouble(3, t.getTotal());
+            ps.setDouble(4, t.getImpuesto());
+            isCreated = (ps.executeUpdate() == 1) ? !isCreated : isCreated;
+        } catch (Exception e) {
+            System.out.println("Error VentaDAO: " + e);
+        } finally {
+            CONEXION.closeConexion();
+        }
+        return isCreated;
+    }
+
+    @Override
+    public VentaDTO read(Integer key) {
+        VentaDTO ventaDTO = new VentaDTO();
+        Connection connection = CONEXION.getConnection();
+        try {
+            PreparedStatement ps = connection.prepareStatement(SQL_READ);
+            ps.setInt(1, key);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                ventaDTO.setId_venta(rs.getInt(1));
+                ventaDTO.setId_usuario(rs.getInt(2));
+                ventaDTO.setId_cliente(rs.getInt(3));
+                ventaDTO.setTotal(rs.getDouble(4));
+                ventaDTO.setFecha(rs.getString(5));
+                ventaDTO.setImpuesto(rs.getDouble(6));
+                ventaDTO.setEstado(rs.getInt(7));
+                
+            }
+        } catch (Exception e) {
+            System.out.println("Error VentaDAO: " + e);
+        } finally {
+            CONEXION.closeConexion();
+        }
+        return ventaDTO;
+    }
+
+    @Override
+    public List<VentaDTO> readAll() {
+        List<VentaDTO> list = new LinkedList<>();
+        Connection connection = CONEXION.getConnection();
+        try {
+            PreparedStatement ps = connection.prepareStatement(SQL_READ_ALL);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                VentaDTO ventaDTO = new VentaDTO();
+                ventaDTO.setId_venta(rs.getInt(1));
+                ventaDTO.setId_usuario(rs.getInt(2));
+                ventaDTO.setId_cliente(rs.getInt(3));
+                ventaDTO.setTotal(rs.getDouble(4));
+                ventaDTO.setFecha(rs.getString(5));
+                ventaDTO.setImpuesto(rs.getDouble(6));
+                ventaDTO.setEstado(rs.getInt(7));
+                list.add(ventaDTO);
+            }
+        } catch (Exception e) {
+            System.out.println("Error VentaDAO: " + e);
+        } finally {
+            CONEXION.closeConexion();
+        }
+        return list;
+    }
+
+    @Override
+    public boolean update(VentaDTO t) {
+        
+    }
+
+    @Override
+    public boolean delete(Integer key) {
+        
+    }
 
 }
