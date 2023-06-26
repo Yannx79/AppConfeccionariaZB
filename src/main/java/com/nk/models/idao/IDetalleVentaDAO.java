26b8e751f98521ba1490d8e5d6e878e42ca3d4bf
[Jose Cardenas] <[josejesuscardenasjimenez@gmail.com]>
2023-06-25 23:35:32 -0500

    IDetalleVentaDAO completo

--- src/main/java/com/nk/models/idao/IDetalleVentaDAO.java
+++ src/main/java/com/nk/models/idao/IDetalleVentaDAO.java
@@ -4,19 +4,14 @@ import com.nk.models.datasource.Conexion;
 
 public interface IDetalleVentaDAO <T, K> extends IObjectDAO<T, K>  {
 
-    public static final String SQL_CREATE = "INSERT INTO `venta`(`id_usuario`, "
-            + "`id_cliente`, `total`,`impuesto`) VALUES "
-            + "(?, ?, ?, ?)";
-    public static final String SQL_READ = "SELECT `id_venta`,`id_usuario`,`id_cliente`,"
-            + "`total`,DATE_FORMAT(`fecha`, '%d/%m/%Y')as `fecha`,"
-            + "`impuesto`,`estado` FROM `venta` WHERE `id_venta` = ?";
-    public static final String SQL_READ_ALL = "SELECT `id_venta`,`id_usuario`,`id_cliente`,"
-            + "`total`,DATE_FORMAT(`fecha`, '%d/%m/%Y')as `fecha`,"
-            + "`impuesto`,`estado` FROM `venta`";
-    public static final String SQL_UPDATE = "UPDATE `venta` SET `id_usuario`=?,"
-            + "`id_cliente`=?,`total`=?,`fecha`=now(),"
-            + "`impuesto`=?,`estado`=? WHERE `id_venta`=?";
-    public static final String SQL_DELETE = "UPDATE `venta` SET `estado` = 0 "
-            + "WHERE `id_venta`=?";
+    public static final String SQL_CREATE = "INSERT INTO `detalle_venta` (`id_producto`,"
+            + "`id_venta`,`subtotal`,`cantidad`) VALUES (?,?,?,?)";
+    public static final String SQL_READ = "SELECT * FROM `detalle_venta` "
+            + "WHERE `id_producto` = ? AND `id_venta` = ?";
+    public static final String SQL_READ_ALL = "SELECT * FROM `detalle_venta`";
+    public static final String SQL_UPDATE = "UPDATE `detalle_venta` SET `subtotal`=?,"
+            + "`cantidad`=?,`estado`=? WHERE id_producto = ? AND id_venta = ?";
+    public static final String SQL_DELETE = "UPDATE `detalle_venta` SET `estado` = 0 "
+            + "WHERE id_producto = ? AND id_venta = ?";
     public static final Conexion CONEXION = Conexion.getConexion();
 }
