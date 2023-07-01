4d5123bf329d98288eed858f8c54cb566900c70e
[Jose Cardenas] <[josejesuscardenasjimenez@gmail.com]>
2023-07-01 12:31:44 -0500

    Primer avance de controlador

--- src/main/java/com/nk/controllers/CAbastecer.java
+++ src/main/java/com/nk/controllers/CAbastecer.java
@@ -1,6 +1,47 @@
-
 package com.nk.controllers;
 
-public class CAbastecer {
+import com.nk.views.VAbastecer;
+import com.nk.models.dto.InsumoDTO;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class CAbastecer implements ActionListener{
+    //frame
+    VAbastecer frame;
+    //Clase de tabla
+    InsumoDTO insDTO;    
+    //Lista de la clase
+    List<InsumoDTO> L_insumos = null;
+    
+    DefaultTableModel modelo = null;
+    public CAbastecer (VAbastecer vista){
+        frame = vista;
+        AgregarBotones(frame);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+    
+    public void AgregarBotones(VAbastecer vista){
+        frame.btnActualizar.addActionListener(this);
+        frame.btnConsultar.addActionListener(this);
+        frame.btnEliminar.addActionListener(this);
+        frame.btnListar.addActionListener(this);
+        frame.btnRegistrar.addActionListener(this);
+    }
+    
+    public DefaultTableModel PonerCabeceras(JTable tabla){
+        String[] titulos={"Id Insumo","Nombre","Descripción","Precio",
+                        "Stock","Estado","F. Inserción","F. Actualización",
+                        "F. Eliminación","Descuento"};
+        DefaultTableModel modelo= new DefaultTableModel(null,titulos);
+        tabla.setModel(modelo);
+        return modelo;
+    }
 
 }
