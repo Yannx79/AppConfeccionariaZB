6adb73318595b8a99814ef43bb2db66984694db5
[Jose Cardenas] <[josejesuscardenasjimenez@gmail.com]>
2023-07-02 23:19:51 -0500

    Cambios para función actualizar y avance en controlador

--- src/main/java/com/nk/models/idao/IInsumoDAO.java
+++ src/main/java/com/nk/models/idao/IInsumoDAO.java
@@ -10,9 +10,7 @@ public interface IInsumoDAO<T, K> extends IObjectDAO<T, K> {
     public static final String SQL_READ = "SELECT * FROM `insumo` WHERE `id_insumo` = ?";
     public static final String SQL_READ_ALL = "SELECT * FROM `insumo` WHERE 1";
     public static final String SQL_UPDATE = "UPDATE `insumo` SET `nombre`=?,"
-            + "`descripcion`=?,`precio`=?,`stock`=?,"
-            + "`estado`=?,`f_insercion`=?,"
-            + "`f_actualizacion`=?,`f_eliminacion`=?,"
+            + "`descripcion`=?,`precio`=?,`stock`=?,`estado`=?,`f_actualizacion`=curdate(),"
             + "`descuento`=? WHERE `id_insumo`=?";
     public static final String SQL_DELETE = "UPDATE `insumo` SET `estado` = 0 "
             + "WHERE `id_insumo`=?";
