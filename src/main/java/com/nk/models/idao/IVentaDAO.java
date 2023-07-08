6a9a0f0238716d1ddccd103b5e7f9a58a8607469
EDGARNPO <episcoche@ucvvirtual.edu.pe>
2023-07-08 18:57:57 -0500

    Controlador Venta y Movimientos Internos

--- src/main/java/com/nk/models/idao/IVentaDAO.java
+++ src/main/java/com/nk/models/idao/IVentaDAO.java
@@ -7,12 +7,8 @@ public interface IVentaDAO<T, K> extends IObjectDAO<T, K> {
     public static final String SQL_CREATE = "INSERT INTO `venta`(`id_usuario`, "
             + "`id_cliente`, `total`,`impuesto`) VALUES "
             + "(?, ?, ?, ?)";
-    public static final String SQL_READ = "SELECT `id_venta`,`id_usuario`,`id_cliente`,"
-            + "`total`,DATE_FORMAT(`fecha`, '%d/%m/%Y')as `fecha`,"
-            + "`impuesto`,`estado` FROM `venta` WHERE `id_venta` = ?";
-    public static final String SQL_READ_ALL = "SELECT `id_venta`,`id_usuario`,`id_cliente`,"
-            + "`total`,DATE_FORMAT(`fecha`, '%d/%m/%Y')as `fecha`,"
-            + "`impuesto`,`estado` FROM `venta`";
+    public static final String SQL_READ = "SELECT * FROM `venta` WHERE `id_venta` = ?";
+    public static final String SQL_READ_ALL = "SELECT * FROM `venta` WHERE 1";
     public static final String SQL_UPDATE = "UPDATE `venta` SET `id_usuario`=?,"
             + "`id_cliente`=?,`total`=?,`fecha`=now(),"
             + "`impuesto`=?,`estado`=? WHERE `id_venta`=?";
