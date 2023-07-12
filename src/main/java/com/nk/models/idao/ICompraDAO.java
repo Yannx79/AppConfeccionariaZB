909deb679185f5008b67b4f62c2699dfa4258718
[Jose Cardenas] <[josejesuscardenasjimenez@gmail.com]>
2023-07-12 17:16:39 -0500

    Ahora si, Ficha compra completo

--- src/main/java/com/nk/models/idao/ICompraDAO.java
+++ src/main/java/com/nk/models/idao/ICompraDAO.java
@@ -7,7 +7,9 @@ public interface ICompraDAO<T, K> extends IObjectDAO<T, K> {
     public static final String SQL_CREATE = "INSERT INTO `compra`(`id_proveedor`, "
             + "`id_usuario`,`total`,`fecha`,`impuesto`,`estado`) VALUES "
             + "(?, ?, ?, ?, ?, ?)";
-    public static final String SQL_READ = "SELECT * FROM `compra` WHERE `id_compra` = ?";
+    public static final String SQL_READ = "SELECT `id_compra`,`id_proveedor`,"
+            + "`id_usuario`,`total`,DATE_FORMAT(`fecha`, '%d/%m/%Y') as `fecha`,"
+            + "`impuesto`,`estado` FROM `compra` WHERE `id_compra` = ?";
     public static final String SQL_READ_ALL = "SELECT `id_compra`,`id_proveedor`,"
             + "`id_usuario`,`total`,DATE_FORMAT(`fecha`, '%d/%m/%Y') as `fecha`,"
             + "`impuesto`,`estado` FROM `compra`";
